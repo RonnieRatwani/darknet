29c5eb35fe2f76577eab9a46052d5d1add766c98
RonnieRatwani <68876566+RonnieRatwani@users.noreply.github.com>
2021-01-31 18:29:55 -0500

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -131,11 +131,12 @@ def print_detections(detections, coordinates=False):
 def draw_boxes(detections, image, colors):
     import cv2
     for label, confidence, bbox in detections:
-        left, top, right, bottom = bbox2points(bbox)
-        cv2.rectangle(image, (left, top), (right, bottom), colors[label], 2)
-        cv2.putText(image,"Hello, World!!",
-                    (left, top - 12), cv2.FONT_HERSHEY_PLAIN, 1,
-                    colors[label], 2)
+        if label == 'car' or label == 'paerrson'
+            left, top, right, bottom = bbox2points(bbox)
+            cv2.rectangle(image, (left, top), (right, bottom), colors[label], 2)
+            cv2.putText(image,"{} [{:.2f}]".format(label, float(confidence)),
+                        (left, top - 12), cv2.FONT_HERSHEY_PLAIN, 1,
+                        colors[label], 2)
     return image
 # "{} [{:.2f}]".format(label, float(confidence))
 
